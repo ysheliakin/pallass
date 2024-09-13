c829e4cf7b5ca249853772a8c67ba57b6054838b
Yaroslav Sheliakin <ysheliakin@ufl.edu>
2024-09-13 01:12:26 -0500

    fix react deployment

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tsconfigPaths()],
+  base: mode === 'production' ? 'https://ysheliakin.github.io/pallass/' : './',
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
   },
-});
+}));
