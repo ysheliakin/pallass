8c27af4135f24f5b7fa4fd2e2adea60f03865192
CamSanti16 <camerons3700@gmail.com>
2024-10-13 22:43:02 -0400

    beginnings of notification frontend (component)

---
+++ client/src/api/notification.ts
@@ -0,0 +1,34 @@
+/*import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+
+const NotificationComponent: React.FC = () => {
+    const [notifications, setNotifications] = useState<any[]>([]);
+
+    useEffect(() => {
+        // Listen for new comments
+        socket.on('new-comment', (data) => {
+            setNotifications((prev) => [...prev, data]);
+        });
+
+        return () => {
+            socket.off('new-comment');
+        };
+    }, []);
+
+    return (
+        <div>
+            <h2>Notifications</h2>
+            <ul>
+                {notifications.map((notification, index) => (
+                    <li key={index}>
+                        User {notification.userId} commented on post {notification.postId}: {notification.comment}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default NotificationComponent;*/
